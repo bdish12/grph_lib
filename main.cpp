82dfa02bf208c0f56fb4bd6e52043aa2001e5676
a.p.evsyukov <a.p.evsyukov@tinkoff.ru>
2023-06-26 03:11:21 +0700

    add code

--- main.cpp
+++ main.cpp
@@ -5,20 +5,8 @@
 
 int main() {
     auto matrix = grph::utils::TGFParser::parseFile(
-            "../Google_tests/integration_tests/test_graphs/example_1.tgf"
+            "../Google_tests/integration_tests/test_graphs/example_from_manual.tgf"
     );
 
     std::cout << "ATR: " << grph::graph::random::ATR::calculateATR(*matrix) << std::endl;
-
-
-    // TODO проверка полного графа на 10 вершинах
-//    auto matrix = grph::graph::AdjacencyMatrix<grph::graph::random::EdgeProbability>(10);
-//
-//    for (int lineIdx = 0; lineIdx < 10; ++lineIdx) {
-//        for (int columnIdx = 0; columnIdx < 10; ++columnIdx) {
-//            if (lineIdx != columnIdx) {
-//                matrix.setAt(0.5, lineIdx, columnIdx);
-//            }
-//        }
-//    }
 }
